8df0d56150feed62167fe5a5f3c0cc3f9f44fb84
Demis Rincon <darmfma@gmail.com>
2024-10-09 11:18:04 -0600

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,6 @@ const nextConfig = {
     compiler:{
         styledComponents: true,
     },
-    reactStrictMode: true,
-  swcMinify: true,
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
